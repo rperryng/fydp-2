cfdaa2bd645163c927e7f57fa11d1a00b7506fa8
Kevin Yang <k46yang@uwaterloo.ca>
2018-03-19 19:54:49 -0400

    Added reference points, cleaned up unused variables

--- BodyLandmarkRecognizer.h
+++ BodyLandmarkRecognizer.h
@@ -40,42 +40,15 @@ private:
 		TP_LeftOuterQuad,
 		TP_RightOuterQuad,
 
-		TP_Count 
+		TP_Count
 	} TracePoints;
 
 	static const TracePoints cWhiteList_Shirt[];
 	static const TracePoints cWhiteList_Shorts[];
 
 	static const int cNumTracePointsShirt = 12;
-	cv::Point m_tracePointsShirt[12];
-	typedef enum _TracePointsShirt {
-		TPS_LeftNeck,
-		TPS_RightNeck,
-		TPS_LeftShoulder,
-		TPS_RightShoulder,
-		TPS_LeftOuterHem,
-		TPS_LeftInnerHem,
-		TPS_RightOuterHem,
-		TPS_RightInnerHem,
-		TPS_LeftHip,
-		TPS_RightHip,
-		TPS_LeftHipUpper,
-		TPS_RightHipUpper
-	} TracePointsShirt;
 
 	static const int cNumTracePointsShorts = 9;
-	cv::Point m_tracePointsShorts[cNumTracePointsShorts];
-	typedef enum _TracePointsShorts {
-		TPSH_LeftHip,
-		TPSH_RightHip,
-		TPSH_Crotch,
-		TPSH_LeftOuterKnee,
-		TPSH_LeftInnerKnee,
-		TPSH_RightOuterKnee,
-		TPSH_RightInnerKnee,
-		TPSH_LeftOuterQuad,
-		TPSH_RightOuterQuad
-	} TracePointsShorts;
 
 	UINT16 *m_depthBuffer;
 	int m_depthBufferHeight, m_depthBufferWidth;
